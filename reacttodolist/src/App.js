8dd7f5710465fc1274a7f5222dd0f77b605db241
Thibaut Humblet <thibaut.humblet@telenet.be>
2020-08-27 13:57:52 +0200

    Made basic todo app with check-able boxes

--- reacttodolist/src/App.js
+++ reacttodolist/src/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Thibaut Humblet - Todo List App</h1>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Basic refresh .NET',
+        completed: true
+      },
+      {
+        id: 2,
+        title: 'Finish thumblet.be website',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Learn React',
+        completed: false
+      },
+    ] 
+  }
+
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map( todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Thibaut Humblet - Todo List App</h1>
+        <Todos todos={this.state.todos} markComplete={this.markComplete}></Todos>
+      </div>
+    );
+  }
+  }
+
 
 export default App;
